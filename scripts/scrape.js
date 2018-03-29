ebcc1948c1fe30fccaf6645bdf05f0ed2683e1ef
wikiwoo <wikiwoo@wikiwoos-MacBook-Air.local>
2018-03-29 11:49:05 -0500

    NYT Scrapper

---
+++ scripts/scrape.js
@@ -0,0 +1,35 @@
+var request = require("request");
+var cheerio = require("cheerio");
+
+var scrape = function(cb) {
+
+	request("https://www.nytimes.com", function(err, res, body) {
+
+		var $ = cheerio.load(body);
+
+		var articles = [];
+
+		$(".theme-summary").each(function(i, element) {
+
+			var head = $(this).children(".story-heading").text().trim();
+			var sum = $(this).children(".summary").text().trim();
+
+			if(head && sum) {
+
+				//Clean up Text with Whitespace on Regex
+				var headNeat = head.replace(/(\r\n|\r|\t|\s+)/gm, " ").trim();
+				var sumNeat = sum.replace(/(\r\n|\r|\t|\s+)/gm, " ").trim();
+
+				var dataToAdd = {
+					headline: headNeat,
+					summary: sumNeat
+				};
+
+				articles.push(dataToAdd)
+			}
+		});
+		cb(articles);
+	});
+};
+
+module.exports = scrape;
